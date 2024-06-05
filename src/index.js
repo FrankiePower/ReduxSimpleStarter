5d26f0992d41518c5a007aa248235f1607b8ce11
FrankiePower <franklin.power21@gmail.com>
2024-06-05 09:06:55 +0100

    react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // Create a new component. This component should produce some HTML
 const App = function () {
   return <div>Hi!</div>;
 };
 
 // Take thus component's generated and put it on the page (in the DOM)
+React.createElement(<App />);

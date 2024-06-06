8c68232a910e87a5252438a1552e6279a38f3d85
FrankiePower <franklin.power21@gmail.com>
2024-06-06 20:19:41 +0100

    Youtube clone

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,14 @@ class Searchbar extends Component {
   }
 
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
+import youtubeApiSearch from "youtube-api-search";
 import Searchbar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_details";
 
 const API_KEY = "AIzaSyBYgFwAznOmzrYg104GIouB50eeZWdvDAs";
 
 // Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <Searchbar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    youtubeApiSearch(
+      { key: API_KEY, term: "How to make a Shawarma" },
+      (videos) => {
+        this.setState({ videos: videos, selectedVideo: videos[0] });
+      }
+    );
+  }
+  render() {
+    return (
+      <div>
+        <Searchbar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 // Take thus component's generated and put it on the page (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector(".container"));
